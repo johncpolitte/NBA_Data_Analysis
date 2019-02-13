3ca11f109925c5ef639b3aa69d78ad093b262566
John Politte <johncpolitte@yahoo.com>
2019-02-13 09:18:48 -0600

    web scraper completed

--- webscrape.py
+++ webscrape.py
@@ -18,24 +18,28 @@ url = 'https://www.basketball-reference.com/leagues/NBA_'
 
 def url_gen(url):
     url_list = []
-    for i in range(1979, 2019):
+    for i in range(2000, 2019):
         url_list.append(url + str(i) + '.html')
     return url_list
 
 season_urls = url_gen(url)
-
+print(season_urls)
 
 def scrape(season_urls):
-    
+    season = 2000
     for i in season_urls:
-        i = {}
+        fields= {}
+        
         webpage = requests.get(season_urls[i])
-        i['misc_tables'] = str(webpage.find_all('div', id="all_misc_stats")[0])
-        i['team_stats_table'] = str(webpage.find_all('div', id='all_team-stats-base')[0])
-        i['shooting_table'] = str(webpage.find_all('div', id='all_team_shooting')[0])
-        with open('scrape_records.log', 'a+') as log:
-            log.write(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
-            log.write('Webpage: {},'.format(webpage))
         soup = BeautifulSoup(webpage.text, 'lxml')
-        mongo_connect.insert_one(i)
+        fields['misc_tables'] = str(webpage.find_all('div', id="all_misc_stats")[0])
+        fields['team_stats_table'] = str(webpage.find_all('div', id='all_team-stats-base')[0])
+        fields['shooting_table'] = str(webpage.find_all('div', id='all_team_shooting')[0])
+        fields['season'] = season
+        season +=1
+        with open('scrape_records.log', 'a+') as log:
+             log.write(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+             log.write('Webpage: {},'.format(webpage))
+        
+        mongo_connect.insert_one(fields)
         time.sleep(60)
