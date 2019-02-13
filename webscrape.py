ff1f49d28f4182092e8bf9d50372d67630273285
John Politte <johncpolitte@yahoo.com>
2019-02-13 10:55:50 -0600

    beginning to read html in from mongo db and completed the data scrape

--- webscrape.py
+++ webscrape.py
@@ -12,34 +12,37 @@ import argparse
 
 client = MongoClient()
 database = client['NBA_tables']   # Database name
-mongo_connect = database['season_page'] 
-
-url = 'https://www.basketball-reference.com/leagues/NBA_'
+mongo_connect = database['seasons'] 
 
 def url_gen(url):
-    url_list = []
-    for i in range(2000, 2019):
-        url_list.append(url + str(i) + '.html')
-    return url_list
+        url_list = []
+        for i in range(2001, 2019):
+            url_list.append(url + str(i) + '.html')
+        return url_list
 
-season_urls = url_gen(url)
-print(season_urls)
 
 def scrape(season_urls):
-    season = 2000
+    season = 2001
     for i in season_urls:
         fields= {}
-        
-        webpage = requests.get(season_urls[i])
+        webpage = requests.get(i)
         soup = BeautifulSoup(webpage.text, 'lxml')
-        fields['misc_tables'] = str(webpage.find_all('div', id="all_misc_stats")[0])
-        fields['team_stats_table'] = str(webpage.find_all('div', id='all_team-stats-base')[0])
-        fields['shooting_table'] = str(webpage.find_all('div', id='all_team_shooting')[0])
+        fields['misc_tables'] = str(soup.find_all('div', id="all_misc_stats")[0])
+        fields['team_stats_table'] = str(soup.find_all('div', id='all_team-stats-base')[0])
+        fields['shooting_table'] = str(soup.find_all('div', id='all_team_shooting')[0])
+
         fields['season'] = season
         season +=1
         with open('scrape_records.log', 'a+') as log:
              log.write(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
-             log.write('Webpage: {},'.format(webpage))
+             log.write('Webpage: {},'.format(i))
         
         mongo_connect.insert_one(fields)
-        time.sleep(60)
+        time.sleep(30)
+
+
+if __name__ =="__main__":
+    url = 'https://www.basketball-reference.com/leagues/NBA_'
+    
+    urls = url_gen(url)
+    scrape(urls)
